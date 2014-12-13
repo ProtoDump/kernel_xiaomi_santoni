43ce26a8faec0f9559538e748f344525b11beb78
Michele Curti <michele.curti@gmail.com>
2014-12-12 16:56:35 -0800

    mm/memcontrol.c: fix defined but not used compiler warning
    test_mem_cgroup_node_reclaimable() is used only when MAX_NUMNODES > 1, so
    move it into the compiler if statement
    
    [akpm@linux-foundation.org: clean up layout]
    Signed-off-by: Michele Curti <michele.curti@gmail.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1597,6 +1597,8 @@ static void mem_cgroup_out_of_memory(struct mem_cgroup *memcg, gfp_t gfp_mask,
 			 NULL, "Memory cgroup out of memory");
 }
 
+#if MAX_NUMNODES > 1
+
 /**
  * test_mem_cgroup_node_reclaimable
  * @memcg: the target memcg
@@ -1619,7 +1621,6 @@ static bool test_mem_cgroup_node_reclaimable(struct mem_cgroup *memcg,
 	return false;
 
 }
-#if MAX_NUMNODES > 1
 
 /*
  * Always updating the nodemask is not very good - even if we have an empty
