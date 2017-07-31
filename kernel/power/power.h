a4db059f543f4621c6fa9fc2c09b4614cf41368c
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-07-31 23:43:18 +0200

    ACPI / PM: Prefer suspend-to-idle over S3 on some systems
    Modify the ACPI system sleep support setup code to select
    suspend-to-idle as the default system sleep state if
    (1) the ACPI_FADT_LOW_POWER_S0 flag is set in the FADT and
    (2) the Low Power Idle S0 _DSM interface has been discovered and
    (3) the default sleep state was not selected from the kernel command
    line.
    
    The main motivation for this change is that systems where the (1) and
    (2) conditions are met typically ship with OSes that don't exercise
    the S3 path in the platform firmware which remains untested and turns
    out to be non-functional at least in some cases.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Tested-by: Mario Limonciello <mario.limonciello@dell.com>

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -179,7 +179,6 @@ extern void swsusp_show_speed(struct timeval *, struct timeval *,
 extern const char * const pm_labels[];
 extern const char *pm_states[];
 extern const char *mem_sleep_states[];
-extern suspend_state_t mem_sleep_current;
 
 extern int suspend_devices_and_enter(suspend_state_t state);
 #else /* !CONFIG_SUSPEND */
